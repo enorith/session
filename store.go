6d24935c413ce6bdb6b39647b2e8c013b7053408
CaoJiayuan <120106578@qq.com>
2022-04-28 11:14:21 +0800

    update store

--- store.go
+++ store.go
@@ -3,6 +3,7 @@ package session
 import (
 	"errors"
 	"io"
+	"sync"
 
 	"github.com/vmihailenco/msgpack/v5"
 )
@@ -13,11 +14,15 @@ var (
 
 type SessionID string
 
+type SessionData map[string][]byte
+
 type Store[T interface{}] struct {
 	Handler Handler
 	ID      SessionID
 	raw     []byte
-	data    *T
+	data    SessionData
+	decoded map[string]interface{}
+	mu      sync.RWMutex
 	started bool
 }
 
@@ -32,11 +37,32 @@ func (s *Store[T]) Start() error {
 }
 
 func (s *Store[T]) Save() error {
-	row, e := msgpack.Marshal(*s.data)
+	if e := s.mergeDecoded(); e != nil {
+		return e
+	}
+
+	row, e := msgpack.Marshal(s.data)
 	if e != nil {
 		return e
 	}
 	return s.Handler.Write(string(s.ID), row)
+}
+
+func (s *Store[T]) mergeDecoded() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.data == nil {
+		s.data = make(SessionData)
+	}
+
+	for k, v := range s.decoded {
+		row, e := msgpack.Marshal(v)
+		if e != nil {
+			return e
+		}
+		s.data[k] = row
+	}
+	return nil
 }
 
 func (s *Store[T]) loadSession() error {
@@ -45,22 +71,62 @@ func (s *Store[T]) loadSession() error {
 	if err != nil {
 		return err
 	}
+	data := make(SessionData)
 
-	var data T
 	err = msgpack.Unmarshal(s.raw, &data)
 	if err != nil && err != io.EOF {
 		return err
 	}
-	s.data = &data
+	s.data = data
 	s.started = true
 	return nil
 }
 
-func (s *Store[T]) Get() (*T, error) {
+func (s *Store[T]) Get(key string) (*T, error) {
 	if !s.started {
 		return nil, ErrNotStarted
 	}
-	return s.data, nil
+
+	if t, ok := s.getDecoded(key); ok {
+		return t, nil
+	}
+	var result T
+
+	s.mu.RLock()
+	raw, ok := s.data[key]
+	s.mu.RUnlock()
+	if ok {
+		err := msgpack.Unmarshal(raw, &result)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+	}
+	s.setDecoded(key, &result)
+	return &result, nil
+}
+
+func (s *Store[T]) getDecoded(key string) (*T, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	if dec, ok := s.decoded[key]; ok {
+		return dec.(*T), ok
+	}
+
+	return nil, false
+}
+
+func (s *Store[T]) setDecoded(key string, value *T) error {
+	if !s.started {
+		return ErrNotStarted
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.decoded == nil {
+		s.decoded = make(map[string]interface{})
+	}
+	s.decoded[key] = value
+	return nil
 }
 
 func NewStore[T interface{}](handler Handler, id string) *Store[T] {
