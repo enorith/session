6ebbd2b2aa753a7fd210dc1097e5824fc0bedb2d
CaoJiayuan <120106578@qq.com>
2022-04-28 15:10:23 +0800

    update session

---
+++ manager.go
@@ -0,0 +1,142 @@
+package session
+
+import (
+	"errors"
+	"io"
+	"sync"
+	"time"
+
+	"github.com/vmihailenco/msgpack/v5"
+)
+
+var (
+	ErrNotStarted = errors.New("session not started")
+)
+
+type Manager struct {
+	Handler    Handler
+	sessions   map[string]*Session
+	rowSession map[string]SessionData
+	started    map[string]bool
+	mu         sync.RWMutex
+}
+
+func (s *Manager) Start(id string) error {
+	if s.started[id] {
+		return nil
+	}
+
+	err := s.Handler.Init(id)
+	if err == nil {
+		return s.loadSession(id)
+	}
+
+	return err
+}
+
+func (s *Manager) Save(id string) error {
+	if e := s.prepareSessions(id); e != nil {
+		return e
+	}
+
+	row, e := msgpack.Marshal(s.rowSession[id])
+	if e != nil {
+		return e
+	}
+	return s.Handler.Write(id, row)
+}
+
+func (s *Manager) prepareSessions(id string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if de, ok := s.sessions[id]; ok {
+		rs, e := de.prepareForSave()
+		if e != nil {
+			return e
+		}
+		s.rowSession[id] = rs
+	}
+
+	return nil
+}
+
+func (s *Manager) loadSession(id string) error {
+	var (
+		err error
+		raw []byte
+	)
+	raw, err = s.Handler.Read(id)
+	if err != nil {
+		return err
+	}
+
+	data := make(SessionData)
+
+	err = msgpack.Unmarshal(raw, &data)
+	if err != nil && err != io.EOF {
+		return err
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	session := Session{
+		ID:      id,
+		TS:      time.Now(),
+		mu:      sync.RWMutex{},
+		decoded: map[string]interface{}{},
+		rowData: data,
+	}
+	var ts int64
+	session.Get("_ts", &ts)
+	if ts > 0 {
+		session.TS = time.Unix(ts, 0)
+	}
+	s.sessions[id] = &session
+	s.rowSession[id] = data
+	s.started[id] = true
+	return nil
+}
+
+func (s *Manager) Get(id string) *Session {
+	session := Session{
+		ID: id,
+	}
+
+	if !s.started[id] {
+		return &session
+	}
+
+	s.mu.RLock()
+	se, ok := s.sessions[id]
+	s.mu.RUnlock()
+	if ok {
+		return se
+	}
+
+	return &session
+}
+
+func (s *Manager) GC(maxLifeTime time.Duration) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for id, session := range s.sessions {
+		if session.TS.Before(time.Now().Add(-maxLifeTime)) {
+			s.sessions[id] = NewSession(id)
+			s.rowSession[id] = make(SessionData)
+		}
+	}
+
+	return s.Handler.GC(maxLifeTime)
+}
+
+func NewManager(handler Handler) *Manager {
+	return &Manager{
+		Handler:    handler,
+		sessions:   make(map[string]*Session),
+		started:    make(map[string]bool),
+		mu:         sync.RWMutex{},
+		rowSession: make(map[string]SessionData),
+	}
+}
--- session.go
+++ session.go
@@ -1,6 +1,15 @@
 package session
 
-import "time"
+import (
+	"io"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/vmihailenco/msgpack/v5"
+)
+
+type SessionData map[string][]byte
 
 type Handler interface {
 	Init(id string) error
@@ -8,4 +17,94 @@ type Handler interface {
 	Write(id string, data []byte) error
 	Destroy(id string) error
 	GC(maxLifeTime time.Duration) error
+}
+
+// Session is a session data
+type Session struct {
+	ID      string
+	TS      time.Time
+	rowData SessionData
+	mu      sync.RWMutex
+	decoded map[string]interface{}
+}
+
+func (s *Session) Get(key string, dest interface{}) error {
+	if s.rowData == nil {
+		return ErrNotStarted
+	}
+
+	if d, ok := s.getDecoded(key); ok {
+		val := reflect.ValueOf(dest)
+		reflect.Indirect(val).Set(reflect.Indirect(reflect.ValueOf(d)))
+		return nil
+	}
+
+	s.mu.RLock()
+	raw, ok := s.rowData[key]
+	s.mu.RUnlock()
+	if ok {
+		err := msgpack.Unmarshal(raw, dest)
+		if err != nil && err != io.EOF {
+			return err
+		}
+	}
+
+	s.setDecoded(key, dest)
+	return nil
+}
+
+func (s *Session) Set(key string, dest interface{}) error {
+	return s.setDecoded(key, dest)
+}
+
+func (s *Session) getDecoded(key string) (interface{}, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	if dec, ok := s.decoded[key]; ok {
+		return dec, ok
+	}
+
+	return nil, false
+}
+
+func (s *Session) setDecoded(key string, value interface{}) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.decoded == nil {
+		s.decoded = make(map[string]interface{})
+	}
+	s.decoded[key] = value
+	return nil
+}
+
+func (s *Session) prepareForSave() (SessionData, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.rowData == nil {
+		s.rowData = make(SessionData)
+	}
+	s.TS = time.Now()
+
+	s.decoded["_ts"] = s.TS.Unix()
+
+	for k, v := range s.decoded {
+		row, e := msgpack.Marshal(v)
+		if e != nil {
+			return nil, e
+		}
+		s.rowData[k] = row
+	}
+
+	return s.rowData, nil
+}
+
+func NewSession(id string) *Session {
+	return &Session{
+		ID:      id,
+		rowData: make(SessionData),
+		mu:      sync.RWMutex{},
+		decoded: make(map[string]interface{}),
+		TS:      time.Now(),
+	}
 }
